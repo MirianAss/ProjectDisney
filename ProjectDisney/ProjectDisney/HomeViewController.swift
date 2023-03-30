4904e5278e8b84fa1be66ca39cdc49c14b8acecf
Mirian Santana <miriansantana93@gmail.com>
2023-03-30 10:14:22 -0300

    Added pod Alamofire and pod SDWebImage. Added class HomeViewController with button connected to navigation

---
+++ ProjectDisney/ProjectDisney/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  ProjectDisney
+//
+//  Created by Mirian Santana on 30/03/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func btStart(_ sender: Any) {
+        if let screen = self.storyboard?.instantiateViewController(withIdentifier: "navigation") {
+            screen.modalPresentationStyle = .fullScreen
+            screen.modalTransitionStyle = .flipHorizontal
+            self.present(screen, animated: true)
+        }
+    }
+}
+
